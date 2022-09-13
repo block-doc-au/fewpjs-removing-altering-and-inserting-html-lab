6d131c4747b4a9f1fa06ffca363a791530f3f560
block-doc-au <neilc@cns-gc.com.au>
2022-09-13 20:28:49 +1000

    NC Completed Removing and altering HTML lab.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+
+newHeader.id = 'victory';
+
+let myContent = 'neilc is the champion';
+newHeader.innerHTML = (myContent);
+
